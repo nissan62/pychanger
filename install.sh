51bf6c68bbeaaceff5ed0c95be7f864a50d53c06
nissan62 <56170498+nissan62@users.noreply.github.com>
2022-02-08 02:21:53 +0600

    first commit
    add "install.sh" and "pychanger" file

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+$(chmod +x pychanger)
+$(sudo cp pychanger /usr/bin/pychanger)
+echo "You can delete the files now"
+echo "To change default python version next time, just type - pychanger"
